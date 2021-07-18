742985f098bca7889e61108a817088d5a4630f8c
juniq <juniqlim@gmail.com>
2021-07-18 22:33:15 +0900

    init

---
+++ src/test/java/im/juniq/board/BoardApplicationTests.java
@@ -0,0 +1,11 @@
+package im.juniq.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
